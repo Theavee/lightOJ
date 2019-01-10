bf54c65c922f93eb8df2846b5a4e4a18500df01f
theavee <aveechakra@gmail.com>
2019-01-11 00:15:29 +0600

    day-six problem update

--- hackerrank/day-six.py
+++ hackerrank/day-six.py
@@ -1,11 +1,5 @@
-input_number = int(input())
+how_many_times = int(input())
 
-for i in range(input_number):
+for i in range(how_many_times):
     word = str(input())
-    sub_string_even , sub_string_odd = ''
-    for j in range(len(word)):
-        if word.index[j] % 2 == 0:
-            sub_string_even = sub_string_even+ word[j]
-        elif word.index[j] %2 ==1:
-            sub_string_odd = sub_string_odd + word[j]
-    print('{}  {}'.format(sub_string_even,sub_string_odd))       
+    print('{}  {}'.format(word[::2], word[1::2]))
